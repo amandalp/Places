3a87a29c9c46015f18408a6dc71531924f115f67
Amanda Peterson <amandaleighp@gmail.com>
2015-04-27 14:54:12 -0700

    Started basic foundation with welcome controller and view with bootstrap

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def set_location(query, id)
+    @places_query = query
+    @places_id    = id 
+  end
+end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,40 @@
+module ApplicationHelper
+	def bootstrap_class_for flash_type
+	  case flash_type
+	    when :success
+	      "alert-success"
+	    when :error
+	      "alert-error"
+	    when :alert
+	      "alert-block"
+	    when :notice
+	      "alert-info"
+	    else
+	      flash_type.to_s
+	  end
+	end
+
+	def search(location)
+	  GooglePlacesApi.query(location)
+	end
+
+	def location_name(location)
+	  search(location)["formatted_address"]
+	end
+
+	def exact_address(location)
+	  search(location)["exact_address"]
+	end
+
+	def destination(location)
+	  search(location)["formatted_address"]
+	end
+
+	def coordinates(location)
+	  search(location)["geometry"]["location"]
+	end
+
+	def place_id(location)
+	  search(location)["place_id"]
+	end
+end
